c6803c230d507d1166af1cbeebc3371c2b839824
Vla Mai <42vlamai@gmail.com>
2022-05-12 15:55:13 +0400

    fix error

--- main.go
+++ main.go
@@ -12,5 +12,8 @@ func main() {
 		fmt.Fprintf(w, "hello the current time is %s", time.Now())
 	})
 	log.Println("Start server on http://localhost:3000")
-	http.ListenAndServe(":3000", nil)
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		return
+	}
 }
